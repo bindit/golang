9cc261ae9e9497cc7a391bd231a83a35b6ddf395
Kamil kurczak <kamil.kurczak@smtsoftware.com>
2016-01-28 14:46:51 +0100

    main fnc refactor

--- example/main.go
+++ example/main.go
@@ -4,6 +4,15 @@ import "emath"
 import "fmt"
 
 func main() {
+	testFibonnaci()
+	testString()
+}
+
+func testFibonnaci() {
 	f := emath.CalculateOne(10);
 	fmt.Printf("%v", f)
+}
+
+func testString() {
+	fmt.Printf("to do")
 }
