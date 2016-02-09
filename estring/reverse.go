fbffb9add93350a30acb5c5251f0f1bce5b7f44b
Kamil kurczak <kamil.kurczak@smtsoftware.com>
2016-02-09 16:36:35 +0100

    change package name

---
+++ estring/reverse.go
@@ -0,0 +1,11 @@
+package estring
+
+func Reverse(s string) string {
+	sArr := []byte(s)
+	sLength := int(len(sArr))
+	for i, j := 0, sLength - 1; i < sLength/2; i, j = i+1, j-1 {
+		sArr[i], sArr[j] = sArr[j], sArr[i]
+	}
+
+	return string(sArr)
+}
